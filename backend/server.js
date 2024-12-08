303be8157a25ee3f3b64a1432d1ee11fcc8dd70d
KISHORE0459 <kishorebabu200409@gmail.com>
2024-12-08 11:42:45 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -34,12 +34,12 @@ app.use(cors({
   }));
 
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 
 
